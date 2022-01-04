815fbe32b481d87757b2f0beb1cdd49e9831720e
Mopkovka13 <evgeniyneskin13@gmail.com>
2022-01-04 17:45:55 +0300

    Add interfaces

---
+++ Interfaces/IShowcase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.Interfaces
+{
+    public interface IShowcase
+    {
+        uint Id { get; set; }
+        string Name { get; set; }
+        int Capacity { get; set; }
+        DateTime CreateDate { get; set; }
+        DateTime DeleteTime { get; set; }
+
+        void ShowProducts();
+    }
+}
