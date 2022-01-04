e7651456aef65ee1510de684d37d1113f0d36416
Mopkovka13 <evgeniyneskin13@gmail.com>
2022-01-04 17:57:50 +0300

    Test Git

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using MovieStore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.Models
+{
+    public class Product : IProduct
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; }
+        public uint Volume { get; set; }
+        public decimal Price { get; set; }
+        
+    }
+}
